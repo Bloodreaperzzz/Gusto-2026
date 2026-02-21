5c1e6c78942fe6e98094414056dd4c1cf863f294
Lakshya Singh <lakshyasingh20042005@gmail.com>
2026-02-21 13:21:20 +0530

    add Users model and UserRepository interface; implement user insertion test

--- Backend/Backend/src/main/java/com/Backend/Backend/Models/Users.java
+++ Backend/Backend/src/main/java/com/Backend/Backend/Models/Users.java
@@ -1,4 +1,28 @@
 package com.Backend.Backend.Models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import org.springframework.data.mongodb.core.index.Indexed;
+@Document(collection="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Users {
+    @Id
+    private String Id;
+    @Indexed(unique=true)
+    private String username;
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    private String Sport;
+
+
 }
--- Backend/Backend/src/main/java/com/Backend/Backend/Repository/UserRepository.java
+++ Backend/Backend/src/main/java/com/Backend/Backend/Repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.Backend.Backend.Repository;
+import com.Backend.Backend.Models.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
 
-public interface UserRepository {
+public interface UserRepository extends MongoRepository<Users, String> {
+
+    // Find user by email
+    Optional<Users> findByEmail(String email);
+
+    // Check if a user exists by email
+    boolean existsByEmail(String email);
 }
